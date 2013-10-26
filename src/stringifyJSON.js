c92dc632c97f79077697d2ebae4c9063c13dc403
Daniel Chao <daniel.h.chao@gmail.com>
2013-10-26 11:36:16 -0700

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,37 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
+
+//helper function
+var stringifyJSON = function(obj){
+	var findType = function(element){
+		return Object.prototype.toString.apply(element);
+	}
+	var type = findType(obj);
+	switch(type)
+	{
+	case '[object Array]':
+		var collection = []
+		for(var i = 0; i < obj.length; i++){
+			collection.push(stringifyJSON(obj[i]));
+		}
+		return '[' + collection.join(',') + ']';
+	case '[object Object]':
+		var collection = []
+		for (var k in obj){
+			var kType = findType(obj[k]);
+			if (kType !== '[object Function]' && kType !== '[object Undefined]'){
+				collection.push("\"" + k + "\":" + stringifyJSON(obj[k]));
+			}
+		}
+		return '{' + collection.join(',') + '}';
+	case '[object String]':
+		return '"' + obj + '"';
+	case '[object Function]':
+		return '';
+	case '[object Undefined]':
+		return '';
+	default:
+		return String(obj);
+	}
 };
