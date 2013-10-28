da752f916699e02ac6768f3d0ed07aef9383e5f4
Daniel Chao <daniel.h.chao@gmail.com>
2013-10-28 12:14:49 -0700

    refactored using underscore

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,17 +8,14 @@ var getElementsByClassName = function (className) {
 	var results = [];
 	var $nodes = document.body.childNodes;
 	var findClass = function(nodes){
-		for(var i = 0; i < nodes.length; i++){
-			var node = nodes[i];
-			if(node.classList){
-				if(node.classList.contains(className)){
-					results.push(node);
-				}
+		_.each(nodes, function(node){
+			if(node.classList && node.classList.contains(className)){
+				results.push(node);
 			}
 			if(node.hasChildNodes()){
 				findClass(node.childNodes);
 			}
-		}
+		})
 	}
 	findClass($nodes);
 	return results;
