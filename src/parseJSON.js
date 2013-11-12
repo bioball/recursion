a6f33521908d6284441c65ae29dd3f4b12712fd9
Daniel Chao <daniel.h.chao@gmail.com>
2013-11-12 09:34:27 -0800

    made parseJSON work on un-nested arrays, numbers, objects and strings

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,36 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  var firstChar = charType(json[0]);
+  switch(firstChar)
+  {
+  case "Array":
+    return _(json.slice(1, json.length - 1).split(',')).map(function(element){
+      return parseJSON(element);
+    });
+  case "Number":
+    return parseInt(json, 10);
+  case "Object":
+    var results = {};
+    json = json.replace(/\s+/g, '');
+    _(json.slice(1, json.length - 1).split(',')).each(function(element){
+      var index = element.indexOf(':');
+      if(index > -1)
+        results[parseJSON(element.substr(0, index))] = parseJSON(element.substr(index + 1, element.length));
+    });
+    return results;
+  case "String":
+    return json.slice(1, json.length-1);
+  }
+};
+
+var charType = function(character){
+  if(character === "[")
+    return "Array";
+  if(parseInt(character, 10))
+    return "Number";
+  if(character === "{")
+    return "Object";
+  if(character === '"')
+    return "String";
 };
